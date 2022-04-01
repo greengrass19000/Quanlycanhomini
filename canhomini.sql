b54a3b5e5852fd9730582024039747a46f865c6b
Nghia Hoang <nghiax32@gmail.com>
2022-04-01 15:51:53 +0700

    Update canhomini.sql

--- canhomini.sql
+++ canhomini.sql
@@ -1,18 +1,20 @@
 CREATE TABLE `Room` (
-	`roomNo` INT(3) NOT NULL,
+	`roomID` INT(8) NOT NULL AUTO_INCREMENT,
 	`buildingID` INT(8) NOT NULL,
 	`floor` INT(2) NOT NULL,
+	`roomNo` INT(3) NOT NULL,
 	`rentalPrice` INT(8) NOT NULL,
 	`description` TEXT NOT NULL,
 	`image` blob,
 	`expectedNumber` INT(1) NOT NULL,
 	`utilities` TEXT NOT NULL,
-	PRIMARY KEY (`roomNo`)
+	PRIMARY KEY (`roomID`)
 );
 
 CREATE TABLE `Contract` (
 	`contractID` INT(8) NOT NULL AUTO_INCREMENT,
-	`roomNo` INT(3) NOT NULL UNIQUE,
+	`roomID` INT(8) NOT NULL UNIQUE,
+	`hostID` INT(8) NOT NULL,
 	`renterID` INT(8) NOT NULL,
 	`deposit` INT(8) NOT NULL,
 	`startTime` DATE NOT NULL,
@@ -22,15 +24,14 @@ CREATE TABLE `Contract` (
 );
 
 CREATE TABLE `Renter` (
-	`renterID` INT(8) NOT NULL AUTO_INCREMENT,
-	`accountID` INT(8) NOT NULL,
+	`renterID` INT(8) NOT NULL,
 	`image` blob,
 	PRIMARY KEY (`renterID`)
 );
 
 CREATE TABLE `Invoice` (
 	`invoiceID` INT(8) NOT NULL AUTO_INCREMENT,
-	`roomNo` INT(2) NOT NULL,
+	`roomID` INT(8) NOT NULL,
 	`type` TEXT NOT NULL,
 	`price` INT(8) NOT NULL,
 	`time` DATE NOT NULL,
@@ -39,8 +40,7 @@ CREATE TABLE `Invoice` (
 );
 
 CREATE TABLE `Host` (
-	`hostID` INT(8) NOT NULL AUTO_INCREMENT,
-	`accountID` INT(8) NOT NULL UNIQUE,
+	`hostID` INT(8) NOT NULL,
 	`image` blob,
 	`property` INT(10) NOT NULL,
 	PRIMARY KEY (`hostID`)
@@ -65,23 +65,28 @@ CREATE TABLE `Account` (
 	PRIMARY KEY (`accountID`)
 );
 
+ALTER TABLE `Room` AUTO_INCREMENT = 10000000;
+
+ALTER TABLE `Contract` AUTO_INCREMENT = 10000000;
+
+ALTER TABLE `Invoice` AUTO_INCREMENT = 10000000;
+
+ALTER TABLE `Building` AUTO_INCREMENT = 10000000;
+
+ALTER TABLE `Account` AUTO_INCREMENT = 10000000;
+
 ALTER TABLE `Room` ADD CONSTRAINT `Room_fk0` FOREIGN KEY (`buildingID`) REFERENCES `Building`(`buildingID`);
 
-ALTER TABLE `Contract` ADD CONSTRAINT `Contract_fk0` FOREIGN KEY (`roomNo`) REFERENCES `Room`(`roomNo`);
+ALTER TABLE `Contract` ADD CONSTRAINT `Contract_fk0` FOREIGN KEY (`roomID`) REFERENCES `Room`(`roomID`);
 
-ALTER TABLE `Contract` ADD CONSTRAINT `Contract_fk1` FOREIGN KEY (`renterID`) REFERENCES `Renter`(`renterID`);
+ALTER TABLE `Contract` ADD CONSTRAINT `Contract_fk1` FOREIGN KEY (`hostID`) REFERENCES `Host`(`hostID`);
 
-ALTER TABLE `Renter` ADD CONSTRAINT `Renter_fk0` FOREIGN KEY (`accountID`) REFERENCES `Account`(`accountID`);
+ALTER TABLE `Contract` ADD CONSTRAINT `Contract_fk2` FOREIGN KEY (`renterID`) REFERENCES `Renter`(`renterID`);
 
-ALTER TABLE `Invoice` ADD CONSTRAINT `Invoice_fk0` FOREIGN KEY (`roomNo`) REFERENCES `Room`(`roomNo`);
+ALTER TABLE `Renter` ADD CONSTRAINT `Renter_fk0` FOREIGN KEY (`renterID`) REFERENCES `Account`(`accountID`);
 
-ALTER TABLE `Host` ADD CONSTRAINT `Host_fk0` FOREIGN KEY (`accountID`) REFERENCES `Account`(`accountID`);
+ALTER TABLE `Invoice` ADD CONSTRAINT `Invoice_fk0` FOREIGN KEY (`roomID`) REFERENCES `Room`(`roomID`);
+
+ALTER TABLE `Host` ADD CONSTRAINT `Host_fk0` FOREIGN KEY (`hostID`) REFERENCES `Account`(`accountID`);
 
 ALTER TABLE `Building` ADD CONSTRAINT `Building_fk0` FOREIGN KEY (`hostID`) REFERENCES `Host`(`hostID`);
-
-
-
-
-
-
-
