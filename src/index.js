4ddfb9abe2473ed441783c49468866d50be0930d
sangvm <vms25062002@gmail.com>
2022-04-13 20:38:56 +0700

    Update

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+var router = express.Router();
+const handlebars = require('express-handlebars');
+const port = 3000;
+const path = require('path');
+
+/*Use static file*/
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*template engine */
+app.engine('handlebars', handlebars.engine({
+  extname: 'handlebars'
+}));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources\\views'));
+
+/* GET home page. */
+console.log(path.join(__dirname, 'resources\\views'));
+app.get('/', (req, res) => {
+  return res.render('home');
+})
+app.get('/news', (req, res) => {
+  return res.render('news');
+})
+
+app.listen(port, () => console.log('Example app listening  at http:://localhost:${port}'))
