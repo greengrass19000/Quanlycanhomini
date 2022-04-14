d61963955278b695fa02ba590feeba0c9371c1f7
greengrass19000 <greengrass19000@gmail.com>
2022-04-14 10:34:28 +0700

    Preparation for Logining accounts

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 var router = express.Router();
 const handlebars = require('express-handlebars');
+const { reset } = require('nodemon');
 const port = 3000;
 const path = require('path');
 
 /*Use static file*/
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded());
+app.use(express.json());
 /*template engine */
 app.engine('handlebars', handlebars.engine({
   extname: 'handlebars'
@@ -24,9 +27,8 @@ app.get('/login', (req, res) => {
   return res.render('login');
 })
 
-app.post('/home', (req, res) => {
-  return res.render('home');
-  //return res.send(req);
+app.post('/login', (req, res) => {
+  return res.send(req.body)
 })
 
 app.listen(port, () => console.log('Example app listening  at http:://localhost:${port}'))
