98f8fa146880787316cd367f5ec7b3d0f75b2b1e
sangvm <vms25062002@gmail.com>
2022-04-23 15:29:57 +0700

    Update

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -2,10 +2,15 @@ const CRUDService = require('../../services/CRUDServices');
 
 class SearchController {
     async search(req, res) {
+        console.log(req.params);
         let data = await CRUDService.getAllRoom();
         return res.render('search', {
             roomsData: data,
         });
+    }
+    async searchDetail(req, res) {
+        console.log(req.params);
+        res.send(req.query);
     }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
 }
 
--- src/routes/search.js
+++ src/routes/search.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const SearchController = require('../app/controllers/SearchController');
 
+router.post('/', SearchController.searchDetail);
 router.get('/', SearchController.search);
 
 module.exports = router;
