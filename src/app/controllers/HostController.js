533a4968b76635774306d9093239b02ca3bc80da
sangvm <vms25062002@gmail.com>
2022-05-07 20:53:45 +0700

    Host update

--- src/app/controllers/HostController.js
+++ src/app/controllers/HostController.js
@@ -2,11 +2,44 @@ const CRUDService = require('../../services/CRUDServices');
 
 class HostController {
     async host(req, res) {
-        let data = await CRUDService.getHostRoom();
+        let data = await CRUDService.getHostRoom(req.query);   
         return res.render('host', {
             roomsData: data,
         });
-    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+    }
+    async postHost(req, res) {
+        console.log("+++++++++++++++");
+        console.log("+++++++++++++++");
+        console.log(req.body);
+        console.log("+++++++++++++++");
+        console.log("+++++++++++++++");
+        return res.render('host', {
+        });
+    }
+    async add(req, res) {
+        let data = await CRUDService.getHostBuilding(req.query);   
+        return res.render('add', {
+            buildingData: data,
+        });
+    }
+    async addRoom(req, res) {
+        let data = await CRUDService.addRoom(req.params);   
+        return res.render('addRoom', {
+            buildingData: data,
+        });
+    }    
+    async edit(req, res) {
+        let data = await CRUDService.getHostRoom(req.query);   
+        return res.render('host', {
+            roomsData: data,
+        });
+    }  
+    async delete(req, res) {
+        let data = await CRUDService.getHostRoom(req.query);   
+        return res.render('host', {
+            roomsData: data,
+        });
+    }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
 }
 
 module.exports = new HostController;
